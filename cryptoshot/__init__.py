a849b9fc801257d65783beb8fc3d6e095cdf8b33
Ross <git@ross.ch>
2026-01-20 16:43:21 +0100

    use version from pyproject

--- cryptoshot/__init__.py
+++ cryptoshot/__init__.py
@@ -1,2 +1,8 @@
-__version__ = "v0.1.0"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("cryptoshot")
+except PackageNotFoundError:
+    __version__ = "0.0.0+unknown"
+
 __description__ = "Retrieve cryptocurrency balances and values at a specific point in time."
